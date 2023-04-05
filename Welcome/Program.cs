8b9698f3b55e26b037d2e69e9fc0fa8b1c3f2b17
Prachi tote <prachi.tote77@gmail.com>
2023-04-05 12:53:23 +0530

    [Prachi] Added welcome message

---
+++ Welcome/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Welcome
+{
+    class Try
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to india");
+
+        }
+    }
+}
